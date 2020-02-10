bd990b41868e042b07153bccbc1065c504829b28
key2-hue <keymaeda3782@gmail.com>
2020-02-11 07:40:22 +0900

    02/11 07:40

--- data.php
+++ data.php
@@ -1,6 +1,14 @@
 <?php
+require_once("city.php");
+require_once("country.php");
+
 $Hawaii = new City("Hawaii","https://tabi-pocket-image.tabikobo.com/wp-content/uploads/2019/09/05151820/9hawaii_beach-1.jpg",5);
 $Paris = new City("Paris","https://storage.tenki.jp/storage/static-images/suppl/article/image/2/29/292/29233/1/large.jpg",4);
 $Auckland = new City("Auckland","https://cdn.britannica.com/99/61399-050-B867F67F/skyline-Auckland-New-Zealand-Westhaven-Marina.jpg",3);
 $London = new City("London","https://www.arukikata.co.jp/com_img/city/large/p_LON.jpg",4);
-$city = array($Hawaii,$Paris,$Auckland,$London);
+
+
+$Finland = new Country("Finland","https://www.jtb.co.jp/kaigai/area/finland/images/fb.jpg",5);
+$Canada = new Country("Canada","https://www.lieugaksquare.com/ca/img/caofficial.jpg",3); 
+$Australia = new Country("Australia","https://x.hankyu-travel.com/cms_photo_image/image_search_kikan5.php?p_photo_mno=00376-webbn-18418.jpg",4);
+$travalInfo = array($Hawaii,$Paris,$Auckland,$London,$Finland,$Canada,$Australia);
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
   require_once("city.php");
   require_once("data.php");
+  require_once('country.php');
 ?>
 <!DOCTYPE html>
 <html>
@@ -12,11 +13,11 @@
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 </head>
 <body>
-  <p>行った国の数<?php echo City::count(); ?>ヶ国</p>
-  <p> <i class="far fa-thumbs-up"></i>が多いほど他の方の満足度は高いです。<p>
+  <p>行った都市の数<?php echo City::count(); ?>ヶ所</p>
+  <p> <i class="far fa-thumbs-up">もしくは<i class="fas fa-globe-asia"></i></i>が多いほど他の方の満足度は高いです。<p>
   <form action="result.php" method="post">
   <div class="country">
-    <?php foreach($city as $c): ?>
+    <?php foreach($travalInfo as $c): ?>
       <div class="oneCountry">
         <div class="countryImage">
           <img src="<?php echo $c->image();?>" class="spotImage">
@@ -24,7 +25,11 @@
         <div class="countryInfo">
           <p><?php echo $c->name(); ?></p>
           <?php for($i = 1;$i < $c->goodPoint();$i++): ?>
-            <i class="far fa-thumbs-up"></i>
+            <?php if($c instanceof City): ?>
+              <i class="far fa-thumbs-up"></i>
+            <?php else: ?>
+              <i class="fas fa-globe-asia"></i>
+            <?php endif ?>
           <?php endfor ?>
         </div>
         <div class="countryReview">
